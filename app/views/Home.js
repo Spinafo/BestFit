06c6551df6f4696a9d1ca1b741c982a6d1627886
testirado <testirado@uoc.edu>
2021-12-17 01:09:17 +0100

    Integrando el componente Menu

--- app/views/Home.js
+++ app/views/Home.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { Menu } from '../widgets/Menu';
 
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
 
 export class Home extends React.Component{
     render(){
         return(
-            <View style={styles.container}>
+            <View style = {styles.container}>
                 <View>
                     <Image source={require("../images/logo-fitness.jpg")}/>
+                    <Menu/>
                 </View>
                 <View>
                     <Text>Zona Clickable</Text>
@@ -18,11 +28,4 @@ export class Home extends React.Component{
     }
 }
 
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+
